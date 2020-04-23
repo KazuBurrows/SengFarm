268884215a5d11e87be185266b4e1e556705bfef
Kazuki Burrows <kazuiscool@gmail.com>
2020-04-23 15:57:11 +1200

    new Game class

---
+++ SengFarm/src/Game.java
@@ -0,0 +1,81 @@
+
+public class Game {
+	
+	private int gameDuration;
+	private int currentDay;
+	
+	private int totalActions;
+	
+	
+	/*
+	 * Constructor
+	 */
+	public Game(int totalDays)
+	{
+		gameDuration = totalDays;
+		currentDay = 0;
+		
+		totalActions = 2;
+		
+		
+		main();
+		
+	}
+	
+	
+	
+	private int getGameDuration()
+	{
+		
+		
+	}
+	
+	
+	private int getCurrentDay()
+	{
+		
+		
+	}
+	
+	
+	private int totalActions()
+	{
+		
+		
+	}
+	
+	
+	private void addAction()
+	{
+		
+		
+	}
+	
+	
+	private void subtractAction()
+	{
+		
+		
+	}
+	
+	
+	
+	private int verifyUserInput()
+	{
+		
+		
+	}
+	
+	
+	
+	private void main()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	
+}
